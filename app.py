c0a3089929b7ac02cea98102ee720383e8e816ca
Dhaxx <kaiopablo44@gmail.com>
2026-08-05 15:06:47 -0300

    feat: implementa novo endpoint /ola_mundo

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -6,3 +9,16 @@ app = FastAPI()
 @app.get('/', status_code=200)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/ola_mundo', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def ola_munde():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,18 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_ola_mundo_retornar_ok_e_html():
+    response = client.get('/ola_mundo')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
